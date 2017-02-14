5350b0ada91189f74a6661350f356f5d6439d5ed
SDK <sdkay@sdkay.pw>
2017-02-14 09:15:45 -0500

    UPDATE
    added a methods for setting headers and cookies, started and
    implementation for setting post, have to change a few things around

--- SRequest/HTTPRequest.cs
+++ SRequest/HTTPRequest.cs
@@ -8,6 +8,61 @@ namespace SRequest
 {
     public class HTTPRequest
     {
+        HttpWebRequest request;
+        Stream requestStream;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        public HTTPRequest(string url)
+        {
+            request = (HttpWebRequest)WebRequest.Create(url);
+        }
+
+        /// <summary>
+        /// Set the headers
+        /// </summary>
+        /// <param name="Headers">List of headers</param>
+        public void SetHeaders(List<HTTPRequestHeader> Headers)
+        {
+            for (int i = 0, n = Headers.Count; i < n; i++)
+                request.Headers.Add(Headers[i].Header, Headers[i].Value);
+        }
+
+        /// <summary>
+        /// Set the cookies
+        /// </summary>
+        /// <param name="Cookies">List of cookies</param>
+        public void SetCookies(List<HTTPRequestCookie> Cookies)
+        {
+            string cookies = string.Empty;
+            for (int i = 0, n = Cookies.Count; i < n; i++)
+                cookies = cookies + $"{Cookies[i].Cookie}={Cookies[i].Value}; ";
+
+            cookies = cookies.Substring(0, cookies.Length - 2);
+            request.Headers.Add("Cookie", cookies);
+        }
+
+        /// <summary>
+        /// Set the post parameters
+        /// </summary>
+        /// <param name="Post">List of params</param>
+        public void SetPost(List<HTTPRequestParams> Post)
+        {
+            string post = string.Empty;
+            for (int i = 0, n = Post.Count; i < n; i++)
+                post = post + $"{Post[i].Param}={Post[i].Value}&";
+            post = post.Substring(0, post.Length - 1);
+            byte[] postBytes = Encoding.ASCII.GetBytes(post);
+            request.Method = "POST";
+            request.ContentLength = postBytes.Length;
+            Stream requestStream = request.GetRequestStream();
+
+            requestStream.Write(postBytes, 0, postBytes.Length);
+            requestStream.Close();
+        }
+
         /// <summary>
         /// Make a HTTP Request
         /// </summary>
@@ -17,57 +72,29 @@ namespace SRequest
         /// <param name="ContentType">ContentType</param>
         /// <param name="Accept">Accept</param>
         /// <param name="Referer">Referer</param>
-        /// <param name="Params">POST Params</param>
-        /// <param name="Cookies">Cookies</param>
-        /// <param name="Headers">Request Headers</param>
-        /// <param name="isPost">Bool for POST or GET</param>
         /// <returns>Source of Req</returns>
-        public string MakeRequest(string URL,
+        public string Execute(
             string Host,
             string UserAgent,
             string ContentType,
             string Accept,
             string Referer,
-            List<HTTPRequestParams> Params,
-            List<HTTPRequestCookie> Cookies, 
-            List<HTTPRequestHeader> Headers, 
-            bool isPost
+            List<HTTPRequestParams> Post,
+            bool IsPost
         )
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-                request.Method = "POST";
                 request.Host = Host.ToString();
                 request.KeepAlive = true;
                 request.UserAgent = UserAgent.ToString();
                 request.ContentType = ContentType.ToString();
                 request.Accept = Accept.ToString();
                 request.Referer = Referer.ToString();
-                string cookies = string.Empty;
-                for (int i = 0, n = Headers.Count; i < n; i++)
-                    request.Headers.Add(Headers[i].Header, Headers[i].Value);
 
-                for (int i = 0, n = Cookies.Count; i < n; i++)
-                    cookies = cookies + $"{Cookies[i].Cookie}={Cookies[i].Value}; ";
-
-                cookies = cookies.Substring(0, cookies.Length - 2);
-                request.Headers.Add("Cookie", cookies);
-
-
-                if (isPost)
+                if(IsPost)
                 {
-                    string post = string.Empty;
-                    for (int i = 0, n = Params.Count; i < n; i++)
-                        post = post + $"{Params[i].Param}={Params[i].Value}&";
-                    post = post.Substring(0, post.Length - 1);
-                    byte[] postBytes = Encoding.ASCII.GetBytes(post);
-                    request.ContentLength = postBytes.Length;
-                    Stream requestStream = request.GetRequestStream();
-
-
-                    requestStream.Write(postBytes, 0, postBytes.Length);
-                    requestStream.Close();
+                    SetPost(Post);
                 }
 
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
@@ -92,28 +119,20 @@ namespace SRequest
         /// <param name="Accept">Accept</param>
         /// <param name="Referer">Referer</param>
         /// <param name="proxy">Proxy</param>
-        /// <param name="Params">POST Params</param>
-        /// <param name="Cookies">Cookies</param>
-        /// <param name="Headers">Request Headers</param>
-        /// <param name="isPost">Bool for POST or GET</param>
         /// <returns>Source of Req</returns>
-        public string MakeRequest(string URL,
+        public string Execute(
            string Host,
            string UserAgent,
            string ContentType,
            string Accept,
            string Referer,
-           WebProxy proxy,
-           List<HTTPRequestParams> Params,
-           List<HTTPRequestCookie> Cookies,
-           List<HTTPRequestHeader> Headers,
-           bool isPost
+           WebProxy proxy, 
+           List<HTTPRequestParams> Post,
+           bool IsPost
        )
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-                request.Method = "POST";
                 request.Host = Host.ToString();
                 request.KeepAlive = true;
                 request.Proxy = proxy;
@@ -121,30 +140,10 @@ namespace SRequest
                 request.ContentType = ContentType.ToString();
                 request.Accept = Accept.ToString();
                 request.Referer = Referer.ToString();
-                string cookies = string.Empty;
-                for (int i = 0, n = Headers.Count; i < n; i++)
-                    request.Headers.Add(Headers[i].Header, Headers[i].Value);
 
-                for (int i = 0, n = Cookies.Count; i < n; i++)
-                    cookies = cookies + $"{Cookies[i].Cookie}={Cookies[i].Value}; ";
-
-                cookies = cookies.Substring(0, cookies.Length - 2);
-                request.Headers.Add("Cookie", cookies);
-
-
-                if (isPost)
+                if (IsPost)
                 {
-                    string post = string.Empty;
-                    for (int i = 0, n = Params.Count; i < n; i++)
-                        post = post + $"{Params[i].Param}={Params[i].Value}&";
-                    post = post.Substring(0, post.Length - 1);
-                    byte[] postBytes = Encoding.ASCII.GetBytes(post);
-                    request.ContentLength = postBytes.Length;
-                    Stream requestStream = request.GetRequestStream();
-
-
-                    requestStream.Write(postBytes, 0, postBytes.Length);
-                    requestStream.Close();
+                    SetPost(Post);
                 }
 
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
